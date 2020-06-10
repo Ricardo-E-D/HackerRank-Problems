a2382ae52e442d7207778ce3af75fc9fedd9f542
Ricardo-E-D <47738496+Ricardo-E-D@users.noreply.github.com>
2020-06-10 11:04:09 +0200

    Solved staircase problem

---
+++ SimpleArraySum/Addition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleArraySum
+{
+    public static class Addition
+    {
+        public static int simpleArraySum(int[] ar)
+        {
+            int sumOfArray = 0;
+
+            foreach (var number in ar)
+            {
+                sumOfArray += number;
+            }
+            return sumOfArray;
+        }
+    }
+}
---
+++ Stair_Case/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Stair_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            staircase(n);
+        }
+        /// <summary>
+        /// n is the size and length of the staircase
+        /// e.g. if n = 3, output should be:
+        ///   x
+        ///  xx
+        /// xxx
+        /// </summary>
+        /// <param name="n"></param>
+        static void staircase(int n)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            
+            /// e.g. of n = 5
+            /// i equals to n(5) - 1 = 4
+            /// first append: ' ' i(4) times
+            /// then append: '#' n(5) - i(4) = 1 time
+            /// lastly make a new line
+            for (int i = n - 1; i >= 0; i--)
+            {
+                sb.Append(' ', i).Append('#', n - i).Append("\n");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
