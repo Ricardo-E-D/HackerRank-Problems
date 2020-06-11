44027a7c050adf305712361060d2d7a9ae64f883
Ricardo-E-D <47738496+Ricardo-E-D@users.noreply.github.com>
2020-06-11 16:18:07 +0200

    Solved Compare the Triplets

---
+++ CompareTheTriplets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompareTheTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> alice = new List<int> { 17, 28, 30 };
+            List<int> Bob = new List<int> { 99, 16, 8 };
+
+            compareTriplets(alice, Bob);
+        }
+
+        static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            List<int> aliceAndBobPoints = new List<int> { 0, 0 };
+
+            if (a.Count == b.Count)
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (a[i] > b[i])
+                    {
+                        aliceAndBobPoints[0]++;
+                    }
+                    else if (a[i] < b[i])
+                    {
+                        aliceAndBobPoints[1]++;
+                    }
+                }
+            }
+            return aliceAndBobPoints;
+        }
+    }
+}
