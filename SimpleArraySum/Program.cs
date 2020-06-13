8f1fa51b5f4ab25a5239fb115e04558d6395c8a3
Ricardo-E-D <47738496+Ricardo-E-D@users.noreply.github.com>
2020-06-13 10:05:15 +0200

    Completed Simple Array Sum

---
+++ SimpleArraySum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 10, 11 };
+
+            int total = simpleArraySum(numbers);
+        }
+
+        /// <summary>
+        /// returns the total sum of all items in the array
+        /// </summary>
+        /// <param name="ar"></param>
+        /// <returns></returns>
+        static int simpleArraySum(int[] ar)
+        {
+            int sum = 0;
+
+            foreach (var num in ar)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
