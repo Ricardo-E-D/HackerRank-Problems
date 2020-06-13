bba598d7b508898c8e1d3e80075d11a3281968d9
Ricardo-E-D <47738496+Ricardo-E-D@users.noreply.github.com>
2020-06-13 09:56:37 +0200

    Completed A Very big Sum

---
+++ AVeryBigSum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AVeryBigSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] numbers = { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
+
+            long sum = aVeryBigSum(numbers);
+        }
+
+        static long aVeryBigSum(long[] ar)
+        {
+            long result = 0;
+
+            foreach (var number in ar)
+            {
+                result += number;
+            }
+            return result;
+        }
+    }
+}
